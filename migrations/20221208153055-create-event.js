bc59b185535127a7d35e38c3d1e2c80ca71a7e7c
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-12-08 10:32:19 -0500

    event model created

---
+++ migrations/20221208153055-create-event.js
@@ -0,0 +1,43 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      date: {
+        type: Sequelize.INTEGER
+      },
+      attending: {
+        type: Sequelize.ARRAY
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      categoryId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('events')
+  }
+}
---
+++ models/event.js
@@ -0,0 +1,30 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Event extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Event.init(
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      date: DataTypes.INTEGER,
+      attending: DataTypes.ARRAY,
+      userId: DataTypes.INTEGER,
+      categoryId: DataTypes.INTEGER
+    },
+    {
+      sequelize,
+      modelName: 'Event',
+      tableName: 'events'
+    }
+  )
+  return Event
+}
