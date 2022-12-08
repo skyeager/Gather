fe1601ca6b780d1f79bb8cc819ab108e3dcd4bed
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-12-08 17:02:15 -0500

    seed file created for events

--- seeders/20221208213553-event.js
+++ seeders/20221208213553-event.js
@@ -1,25 +1,56 @@
-'use strict';
+'use strict'
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'events',
+      [
+        {
+          name: 'Yoga in the park',
+          description: '',
+          date: '',
+          attending: ['BurlingtonAdventurer', 'MusicLover', 'VermontFoodie'],
+          category_id: 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Pick up basketball',
+          description:
+            'Join a friendly game of pick up basketball at Pomeroy Park this Sunday! Bring your sneaks and a water bottle!',
+          date: 07072022,
+          attending: ['BurlingtonAdventurer', 'MusicLover', 'VermontFoodie'],
+          category_id: 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Jazz festival',
+          description:
+            'The best music festival in town! Come enjoy three days of smooth music at your favorite downtown spots!',
+          date: 06032022,
+          attending: ['BurlingtonAdventurer', 'MusicLover', 'VermontFoodie'],
+          category_id: 2,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Five dollar burger night',
+          description:
+            'The famous five dollar burger night is back! Come to Nectar for the cheapiest and tastiest burger in town.',
+          date: 08122022,
+          attending: ['BurlingtonAdventurer', 'MusicLover', 'VermontFoodie'],
+          category_id: 3,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    )
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('events', null, {})
   }
-};
+}
