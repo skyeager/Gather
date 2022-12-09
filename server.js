ed317b99a783cdafb58ad67382541f78d8ddc89b
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-12-09 11:30:31 -0500

    server.js filled out

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
 const AuthRouter = require('./routes/AuthRouter')
-const ReviewRouter = require('./routes/ReviewRouter')
-const RestaurantRouter = require('./routes/RestaurantRouter')
+const CategoryRouter = require('./routes/CategoryRouter')
+const EventRouter = require('./routes/EventRouter')
 const app = express()
 
 const PORT = process.env.PORT || 3001
@@ -14,9 +14,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.json({ msg: `Server Works!` }))
-app.use('/user', AuthRouter)
-app.use('/review', ReviewRouter)
-app.use('/restaurants', RestaurantRouter)
+app.use('/auth', AuthRouter)
+app.use('/category', CategoryRouter)
+app.use('/event', EventRouter)
 
 app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`))
 
