2e9f033a2bd2cc1c225d130ff13525df33cbfbd6
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-12-09 13:44:01 -0500

    user event controller shell created

---
+++ controllers/UserEventController.js
@@ -0,0 +1,20 @@
+const { Category } = require('../models')
+
+const GetUserEvents = async (req, res) => {
+  try {
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetAttendees = async (req, res) => {
+  try {
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  GetUserEvents,
+  GetAttendees
+}
---
+++ routes/UserEventRouter.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const controller = require('../controllers/UserEventController')
+// const middleware = require('../middleware')
+
+
+router.get('/', controller.)
+router.get('/', controller.)
+
+module.exports = router
