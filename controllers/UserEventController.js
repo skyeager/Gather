3fcdfad4703cb6452d37e67bcfd80f11bfbaf82b
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-12-09 13:53:33 -0500

    get attendees controller created

--- controllers/UserEventController.js
+++ controllers/UserEventController.js
@@ -1,7 +1,8 @@
-const { Category } = require('../models')
+const { User, Event } = require('../models')
 
 const GetUserEvents = async (req, res) => {
   try {
+    const userEvents = await res.send(userEvents)
   } catch (error) {
     throw error
   }
@@ -9,6 +10,15 @@ const GetUserEvents = async (req, res) => {
 
 const GetAttendees = async (req, res) => {
   try {
+    const attendees = await Event.findAll({
+      include: [
+        {
+          model: User,
+          as: 'attendee',
+          attributes: ['id', 'username']
+        }
+      ]
+    })
   } catch (error) {
     throw error
   }
