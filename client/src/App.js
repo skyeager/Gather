ad99f05d364031daf9b62ade2c1e3dba8f8a3060
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-12-10 09:27:37 -0500

    app.js auth created

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,45 @@
-import './App.css'
+import { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router'
+import Nav from './components/Nav'
+import Register from './pages/Register'
+import SignIn from './pages/SignIn'
+import Feed from './pages/Feed'
+import Home from './pages/Home'
+import { CheckSession } from './services/Auth'
+import './styles/App.css'
 
-function App() {
+const App = () => {
   const [user, setUser] = useState(null)
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    // Check if token exists before requesting to validate the token
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
+  const handleLogOut = () => {
+    //Reset all auth related state and clear localStorage
+    setUser(null)
+    localStorage.clear()
+  }
 
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Nav user={user} handleLogOut={handleLogOut} />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn setUser={setUser} />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </main>
     </div>
   )
 }
