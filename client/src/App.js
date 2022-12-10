fa774ca21527a1a65a38b997170bfdc78b1e9673
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-12-10 09:37:40 -0500

    signIn.js created

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,11 @@ import { Route, Routes } from 'react-router'
 import Nav from './components/Nav'
 import Register from './pages/Register'
 import SignIn from './pages/SignIn'
-import Feed from './pages/Feed'
 import Home from './pages/Home'
 import { CheckSession } from './services/Auth'
 import './styles/App.css'
+// import CreateEvent from './pages/CreateEvent'
+// import UpdateEvent from './pages/UpdateEvent'
 
 const App = () => {
   const [user, setUser] = useState(null)
@@ -18,14 +19,12 @@ const App = () => {
 
   useEffect(() => {
     const token = localStorage.getItem('token')
-    // Check if token exists before requesting to validate the token
     if (token) {
       checkToken()
     }
   }, [])
 
   const handleLogOut = () => {
-    //Reset all auth related state and clear localStorage
     setUser(null)
     localStorage.clear()
   }
@@ -38,6 +37,28 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn setUser={setUser} />} />
           <Route path="/register" element={<Register />} />
+          {/* <Route
+            path="/"
+            element={
+              <CreateEvent
+                user={user}
+                initialState={initialState}
+                formState={formState}
+                setFormState={setFormState}
+              />
+            }
+          /> */}
+          {/* <Route
+            path=""
+            element={
+              <UpdateEvent
+                user={user}
+                formState={formState}
+                setFormState={setFormState}
+                initialState={initialState}
+              />
+            }
+          /> */}
         </Routes>
       </main>
     </div>
---
+++ client/src/pages/SignIn.js
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import { SignInUser } from '../services/Auth'
+import { useNavigate } from 'react-router-dom'
+
+const SignIn = ({ setUser }) => {
+  let navigate = useNavigate()
+
+  const initialState = { email: '', password: '' }
+
+  const [formValues, setFormValues] = useState(initialState)
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const payload = await SignInUser(formValues)
+    setUser(payload)
+    setFormValues(initialState)
+    navigate('/feed')
+  }
+
+  return (
+    <div className="signin col">
+      <div className="card-overlay centered">
+        <form className="col" onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={handleChange}
+              name="email"
+              type="email"
+              placeholder="example@example.com"
+              value={formValues.email}
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={handleChange}
+              type="password"
+              name="password"
+              value={formValues.password}
+              required
+            />
+          </div>
+          <button disabled={!formValues.email || !formValues.password}>
+            Sign In
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default SignIn
