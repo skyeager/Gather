fa2cffdc27e3c9b3417113c2145a3f33e3675dbc
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-12-10 11:36:02 -0500

    middleware added to event router

--- routes/EventRouter.js
+++ routes/EventRouter.js
@@ -1,12 +1,30 @@
 const router = require('express').Router()
 const controller = require('../controllers/EventController')
-// const middleware = require('../middleware')
+const middleware = require('../middleware')
 
 router.get('/:event_id', controller.GetEvent)
 // router.get('/everyevent', controller.GetAllEvents)
 router.get('/categorylist/:category_id', controller.CategoryListOfEvents)
-router.post('/create/:user_id', controller.CreateEvent)
-router.put('/update/:event_id', controller.UpdateEvent)
-router.delete('/delete/:event_id', controller.DeleteEvent)
+
+router.post(
+  '/create/:user_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreateEvent
+)
+
+router.put(
+  '/update/:event_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdateEvent
+)
+
+router.delete(
+  '/delete/:event_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeleteEvent
+)
 
 module.exports = router
