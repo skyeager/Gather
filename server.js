f3e039636b06d6f6841ff4bf472b47064c682173
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-12-10 10:50:50 -0500

    auth router added to server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
-// const AuthRouter = require('./routes/AuthRouter')
+const AuthRouter = require('./routes/AuthRouter')
 const CategoryRouter = require('./routes/CategoryRouter')
 const EventRouter = require('./routes/EventRouter')
 const UserEventRouter = require('./routes/UserEventRouter')
@@ -16,22 +16,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.json({ msg: `Server Works!` }))
-// app.use('/auth', AuthRouter)
+app.use('/auth', AuthRouter)
 app.use('/category', CategoryRouter)
 app.use('/event', EventRouter)
 app.use('/userevent', UserEventRouter)
 app.use('/user', UserRouter)
 
 app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`))
-
-// const express = require('express')
-// const cors = require('cors')
-// const logger = require('morgan')
-
-// const PORT = process.env.PORT || 3001
-
-// const app = express()
-
-// app.listen(PORT, () => {
-//   console.log(`Express server listening on port ${PORT}`)
-// })
