ffa6b0c12aaefe93ffc6d8bf1672d6d80d5d5999
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-12-14 15:39:14 -0500

    second deploy attempt

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(`${__dirname}/client/build`))
 
 app.get('/', (req, res) => res.json({ msg: `Server Works!` }))
 app.use('/auth', AuthRouter)
@@ -24,4 +25,7 @@ app.use('/userevent', UserEventRouter)
 app.use('/user', UserRouter)
 app.use('/posts', PostRouter)
 
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
 app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`))
