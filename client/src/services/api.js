ad3dc68a36f3b1c7ec0a920e82b3ca041be2473c
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-12-14 20:04:15 -0500

    react app baseurl added to .env

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,8 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'https://gather-vermont.herokuapp.com'
+// export const BASE_URL = 'https://gather-vermont.herokuapp.com'
+
+export const BASE_URL = process.env.REACT_APP_BASEURL
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
