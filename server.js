6714329ff9a57c8b8117ef20143628dbe05196e1
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-12-14 20:17:22 -0500

    cors origin added to server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 
 const PORT = process.env.PORT || 3001
 
-app.use(cors())
+app.use(cors({ origin: ['https://gather-vermont.herokuapp.com/'] }))
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
